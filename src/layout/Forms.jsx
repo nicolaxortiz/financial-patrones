5de34728eee84129d0e93a352848392ce0e59666
nicolaxortiz <nicolaxortiz@gmail.com>
2025-04-17 10:52:30 -0500

    fixed register form

--- src/layout/Forms.jsx
+++ src/layout/Forms.jsx
@@ -4,29 +4,33 @@ import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 import ValidateModal from "../components/ValidateModal";
 import ForgotPasswordForm from "../components/ForgotPasswordForm";
+import Footer from "./Footer";
 
 export default function Forms({ type }) {
-	const [open, setOpen] = useState(false);
-	const [id, setId] = useState();
-	const [status, setStatus] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [id, setId] = useState();
+  const [status, setStatus] = useState(false);
 
-	return (
-		<div
-			className={
-				type === "register" ? "form-layout-register" : "form-layout-login"
-			}
-		>
-			{type === "login" && (
-				<LoginForm setOpen={setOpen} setId={setId} status={status} />
-			)}
-			{type === "register" && <RegisterForm />}
-			{type === "password" && <ForgotPasswordForm />}
-			<ValidateModal
-				setOpen={setOpen}
-				open={open}
-				id={id}
-				setStatus={setStatus}
-			/>
-		</div>
-	);
+  return (
+    <>
+      <div
+        className={
+          type === "register" ? "form-layout-register" : "form-layout-login"
+        }
+      >
+        {type === "login" && (
+          <LoginForm setOpen={setOpen} setId={setId} status={status} />
+        )}
+        {type === "register" && <RegisterForm />}
+        {type === "password" && <ForgotPasswordForm />}
+        <ValidateModal
+          setOpen={setOpen}
+          open={open}
+          id={id}
+          setStatus={setStatus}
+        />
+      </div>
+      {type === "register" && <Footer />}
+    </>
+  );
 }
